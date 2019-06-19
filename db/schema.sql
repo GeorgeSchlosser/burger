f90ce862354aec613fa234ae283bb8b58f8ff73f
Trash Panda <d.george.schlosser@gmail.com>
2019-06-18 23:54:40 -0400

    note about mysql cli

--- db/schema.sql
+++ db/schema.sql
@@ -7,3 +7,8 @@ CREATE TABLE burgers(
     devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
+
+-- Start MySQL command line tool and login: mysql -u root -p.
+-- With the mysql> command line tool running, enter the command source schema.sql. This will run your schema file and all of the queries in it, in other words, you'll be creating your database.
+-- Now insert the entries you defined in seeds.sql by running the file: source seeds.sql.
+-- Close out of the MySQL command line tool: exit.
