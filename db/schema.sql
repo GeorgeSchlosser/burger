b3005cf686377806a2edc2e9761c3384a1329a3e
Trash Panda <d.george.schlosser@gmail.com>
2019-06-20 11:49:30 -0400

    added schema for Heroku/JawsDB

--- db/schema.sql
+++ db/schema.sql
@@ -12,3 +12,12 @@ CREATE TABLE burgers(
 -- With the mysql> command line tool running, enter the command source schema.sql. This will run your schema file and all of the queries in it, in other words, you'll be creating your database.
 -- Now insert the entries you defined in seeds.sql by running the file: source seeds.sql.
 -- Close out of the MySQL command line tool: exit.
+
+-- Schema for JawsDB
+CREATE TABLE burgers(
+    id INT AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR(99) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    createdAt TIMESTAMP NOT NULL,
+    PRIMARY KEY(id)
+);
